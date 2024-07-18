b0bbf9de657ecedb65177d06cdcf16cc3642313a
Daniel Lopez <daniellopez7812@outlook.com>
2024-07-18 17:13:40 -0600

    loadUsers func renamed; new name loadUsersByPage

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -1,4 +1,4 @@
-export const loadUsers = async (page = 1) => {
+export const loadUsersByPage = async (page = 1) => {
   const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
   const res = await fetch(url);
   const data = await res.json();
