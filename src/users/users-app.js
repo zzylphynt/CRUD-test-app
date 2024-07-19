6eec322ddaa90f18393ff86dca2717ad7aa6bb2a
Daniel Lopez <daniellopez7812@outlook.com>
2024-07-18 21:56:10 -0600

    feat(render-table): data table created and rendered in HTML

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,8 +1,9 @@
-import usersStore from "./store/users-store"
+import {renderTable  } from "./presentation/render-table/render-table";
+import usersStore from "./store/users-store";
 
-export const usersApp = async(element) =>{
-
-    element.innerHTML = "Loading..."
-    await usersStore.loadNextPage();
-    console.log(usersStore.getUsers())
-}
+export const usersApp = async (element) => {
+  element.innerHTML = "Loading...";
+  await usersStore.loadNextPage();
+  element.innerHTML = "";
+  renderTable(element);
+};
