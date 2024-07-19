b31eac0300d0bdfd587f0341b920cc9250091c64
Daniel Lopez <daniellopez7812@outlook.com>
2024-07-18 19:08:36 -0600

    refactor(load-users-by-page):add code readability improvements

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -1,18 +1,21 @@
 import { localhostUserToModel } from "../mappers/localhost-user.mapper";
 
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+};
+
 export const loadUsersByPage = async (page = 1) => {
   try {
     const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
 
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error(`HTTP error! status: ${res.status}`);
-    }
-    const { data } = await res.json();
-    const users = data.map(localhostUserToModel);
-    console.log("Mapped users:", users);
-
-    return users;
+    const { data } = await fetchData(url);
+    if (!Array.isArray(data)) throw new Error("invalid data structure");
+    
+    return data.map(localhostUserToModel);
   } catch (error) {
     console.error("Error loading users by page:", error);
     throw error;
